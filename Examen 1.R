594f621c5caf9e929ff05ebd1e805b884ecb6eba
Anny sanchez <Anny_lissethDlc@hotmail.com>
2020-02-26 09:38:04 -0600

    examen 1

---
+++ Examen 1.R
@@ -0,0 +1,35 @@
+
+#Anny sánchez de la cerda
+#matricula : 1811530
+
+
+1 Descripcion -------------------------------------------------------------
+
+#para este examen se tendran dos bases de datos que se describen en cada seccion correspondiente 
+  
+
+2 Correlacion 
+
+2.1 Importar Datos
+
+summary(Historico_)
+
+
+mean(Historico_$`Numero de incendios`)
+
+median(Historico_$`Numero de incendios`)
+
+median(Historico_$`Superficie (ha)`)
+
+fivenum(Historico_$`Numero de incendios`)
+
+
+var(Historico_$`Superficie (ha)`)
+
+# Representacion grafica --------------------------------------------------
+
+
+
+
+boxplot(Historico_$`Numero de incendios`) 
+boxplot(Historico_$`Superficie (ha)`)
